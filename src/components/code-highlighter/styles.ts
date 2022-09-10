b8d81d223f53deb6b8856c1b8c6edb43f4da1ba1
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2022-09-10 17:33:49 +0200

    feat: add code metadata

--- src/components/code-highlighter/styles.ts
+++ src/components/code-highlighter/styles.ts
@@ -2,12 +2,19 @@ import { styled } from "@linaria/react";
 import { Icon } from "../icon";
 
 export const ControlsWrapper = styled.div`
-  margin-top: -24px;
   position: relative;
-  top: 34px;
   margin-left: 14px;
-  margin-right: 0px;
+  margin-right: 14px;
   z-index: 2;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  top: 48px;
+`;
+
+export const WindowTitle = styled.span`
+  font-family: "Fira Code", monospace;
+  font-size: 1rem;
 `;
 
 export const StyledCode = styled.span`
@@ -27,9 +34,6 @@ export const ClipboardCopy = styled.div`
   background-color: rgb(33, 38, 45);
   border: 1px solid rgba(240, 246, 252, 0.1);
   border-radius: 6px;
-  position: absolute;
-  right: 10px;
-  top: 0px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -47,11 +51,11 @@ export const ClipboardCopy = styled.div`
 `;
 
 export const Wrapper = styled.div`
-  margin-top: 32px;
+  margin-top: -12px;
   margin-bottom: 32px;
   .CodeMirror {
     height: auto;
-    padding: 48px 18px 18px 12px;
+    padding: 60px 18px 18px 12px;
     border-radius: 5px;
     box-shadow: 0 2px 6px rgb(0 0 0 / 55%);
     font-family: "Fira Code", monospace;
