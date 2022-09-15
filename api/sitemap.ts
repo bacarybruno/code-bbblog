3f95c48b5cb8165a287e229e9aa11f09ae4d0af9
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2022-09-15 11:23:05 +0200

    chore: sitemap api generator

---
+++ api/sitemap.ts
@@ -0,0 +1,64 @@
+import { request, gql } from "graphql-request";
+
+const staticPages = ["/", "/home", "/about", "/blog", "/projects"];
+
+type BlogPostCollection = {
+  blogPostCollection: { items: { slug: string }[] };
+};
+
+const getDynamicPages = async () => {
+  try {
+    console.log("Going to fetch blog posts slug");
+
+    const spaceId = process.env.VITE_CONTENTFUL_SPACE_ID;
+    const apiKey = process.env.VITE_CONTENTFUL_API_KEY;
+
+    console.log("Going to check env variables");
+
+    if (!spaceId) {
+      throw new Error("`process.env.VITE_CONTENTFUL_SPACE_ID` is missing");
+    }
+
+    if (!apiKey) {
+      throw new Error("`process.env.VITE_CONTENTFUL_API_KEY` is missing");
+    }
+
+    console.log("Successfully checked env variables");
+
+    const query = gql`
+      query FetchPosts {
+        blogPostCollection {
+          items {
+            slug
+          }
+        }
+      }
+    `;
+
+    console.log("Going to send API request");
+    const apiURL = `https://graphql.contentful.com/content/v1/spaces/${spaceId}?access_token=${apiKey}`;
+    const result = await request<BlogPostCollection>(apiURL, query);
+    console.log("Successfully sent API request");
+    return result.blogPostCollection.items.map((item) => `/posts/${item.slug}`);
+  } catch (error) {
+    console.error("An error occured while trying to get posts:", error.message);
+  }
+};
+
+export default async (_request, response) => {
+  const dynamicPages = await getDynamicPages();
+  let pages = staticPages;
+  if (dynamicPages) {
+    pages = pages.concat(dynamicPages);
+  }
+
+  const sitemap = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${pages.map((path) => `<url>
+    <loc>https://bacarybruno.com${path}</loc>
+    <changefreq>daily</changefreq>
+    <priority>0.7</priority>
+  </url>`).join("\n\t")}
+</urlset>`;
+
+  response.status(200).send(sitemap);
+};
