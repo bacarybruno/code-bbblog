10c2a092c41e826f676a7655846b19ed7af7b47f
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2022-10-08 19:13:51 +0200

    chore: add placeholder

--- api/blog.ts
+++ api/blog.ts
@@ -12,7 +12,7 @@ const meta: Meta = {
   title: 'Code-BBBlog | Blog',
   description: '👨🏽‍💻 Hello! I\'m Bruno. A FullStack Developer and Lifelong learner who loves building great products that has a positive impact 🤩 Passionate about Software Engineering 💻, japanese anime\'s 🥷 and building side projects.',
   url: '',
-  image: 'https://placehold.co/900x450/007ACC/FFFFFF?text=Blog&font=Roboto',
+  image: 'https://placehold.co/900x450/007ACC/FFFFFF.webp?text=Blog&font=Roboto',
 };
 
 // TODO: add cache control
--- api/post.ts
+++ api/post.ts
@@ -43,7 +43,7 @@ export default async (request: VercelRequest, response: VercelResponse) => {
     title: `CodeBBBlog | ${post?.title}`,
     description: post?.excerpt,
     url: getBaseUrl(request.url),
-    image: `https://placehold.co/900x450/007ACC/FFFFFF?text=${post?.title}&font=Roboto`,
+    image: `https://placehold.co/900x450/007ACC/FFFFFF.webp?text=${post?.title}&font=Roboto`,
   };
   const stringified = getParsedHTML(meta);
 
