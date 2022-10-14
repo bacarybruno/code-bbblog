a8a98c24a952b4017cf47dcc2757b7a7b48b197c
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2022-10-14 09:42:14 +0200

    feat(blog): generate rss feed

---
+++ api/rss.xml.ts
@@ -0,0 +1,68 @@
+import { request, gql } from 'graphql-request';
+import RSS from 'rss';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { getBaseUrl, getContentfulApiURL } from './helpers';
+
+type BlogPostCollection = {
+  blogPostCollection: {
+    items: {
+      title: string;
+      slug: string;
+      excerpt: string;
+      sys: {
+        publishedAt: string;
+      };
+    }[];
+  };
+};
+
+const getDynamicPages = async () => {
+  try {
+    const query = gql`
+      query FetchPosts {
+        blogPostCollection {
+          items {
+            title
+            slug
+            excerpt
+            sys {
+              publishedAt
+            }
+          }
+        }
+      }
+    `;
+
+    const apiURL = getContentfulApiURL();
+    const result = await request<BlogPostCollection>(apiURL, query);
+    return result.blogPostCollection.items;
+  } catch (error) {
+    return [];
+  }
+};
+
+export default async (_request: VercelRequest, response: VercelResponse) => {
+  const allBlogPosts = await getDynamicPages();
+  const feed = new RSS({
+    title: 'Code-BBBlog by @bacarybruno',
+    site_url: getBaseUrl(),
+    feed_url: `${getBaseUrl()}/feed.xml`,
+  });
+
+  allBlogPosts.map((post) => {
+    feed.item({
+      title: post.title,
+      url: `${getBaseUrl()}/${post.slug}`,
+      date: post.sys.publishedAt,
+      description: post.excerpt
+    });
+  });
+
+  return response
+    .setHeader(
+      'Cache-Control',
+      'public, s-maxage=1200, stale-while-revalidate=600'
+    )
+    .setHeader('Content-Type', 'application/xml')
+    .send(feed.xml({ indent: true }));
+};
